3414937f49b280345edce7f3fbe84249c3d5065a
Saii Anish R <saiianish.2006.sa@gmail.com>
2025-12-29 10:03:43 +0530

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,54 @@
+
+int delay_time=5;
+int redPin=3;
+int greenPin=5;
+int bluePin=6;
+void setup() {
+pinMode(redPin,OUTPUT);
+pinMode(greenPin,OUTPUT);
+pinMode(bluePin,OUTPUT);
+}
+void loop() {
+for(int i=0;i<255;i++)
+{
+analogWrite(redPin,i);
+analogWrite(greenPin,0);
+analogWrite(bluePin,0);
+delay(delay_time);
+}
+for(int i=255;i>0;i--)
+{
+analogWrite(redPin,i);
+analogWrite(greenPin,0);
+analogWrite(bluePin,0);
+delay(delay_time);
+} //Fade RED on and off
+for(int i=0;i<255;i++)
+{
+analogWrite(redPin,0);
+analogWrite(greenPin,i);
+analogWrite(bluePin,0);
+delay(delay_time);
+}
+for(int i=255;i>0;i--)
+{
+analogWrite(redPin,0);
+analogWrite(greenPin,i);
+analogWrite(bluePin,0);
+delay(delay_time);
+} //Fade GREEN on and off
+for(int i=0;i<255;i++)
+{
+analogWrite(redPin,0);
+analogWrite(greenPin,0);
+analogWrite(bluePin,i);
+delay(delay_time);
+}
+for(int i=255;i>0;i--)
+{
+analogWrite(redPin,0);
+analogWrite(greenPin,0);
+analogWrite(bluePin,i);
+delay(delay_time);
+} //Fade BLUE on and off
+}
